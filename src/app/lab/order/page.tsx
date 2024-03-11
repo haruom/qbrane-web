4b4ce17e6ddc29faf3960c88037db7d613d8f6a2
tyukei <0024481294bxyk6037@gmail.com>
2024-03-11 13:24:05 +0900

    update:0311

--- src/app/lab/order/page.tsx
+++ src/app/lab/order/page.tsx
@@ -60,19 +60,35 @@ export default function WalletInteraction() {
     if (!provider) return alert('Ethereum provider not found. Please install MetaMask.');
 
     try {
+      // Returns the current price per gas in wei.
+      const gasPrice = await provider.request({
+        method: 'eth_gasPrice',
+        params: [],
+      });
+      console.log('Gas Price:', gasPrice);
+      console.log('parseInt(gasPrice, 16):', parseInt(gasPrice, 16));
+      console.log('MATIC:', parseInt(gasPrice, 16) / (10 ** 18));
+      // 0.00000001 MATIC
+
       const txParams = {
         from: account,
         to: '0x32cc368b24789d033d95a101da99667c6e94c4a2',
-        value: '0x1',
-        gasPrice: '0x1',
+        value: '0x150000000000000',
+        gasPrice: '0x0',
       };
 
-      const txHash = await provider.request({
-        method: 'eth_sendTransaction',
+      const estimatedGas = await provider.request({
+        method: 'eth_estimateGas',
         params: [txParams],
       });
+      console.log('Estimated Gas:', estimatedGas);
 
-      console.log('Transaction Hash:', txHash);
+      // const txHash = await provider.request({
+      //   method: 'eth_sendTransaction',
+      //   params: [{ ...txParams, gasPrice: estimatedGas }],
+      // });
+
+      // console.log('Transaction Hash:', txHash);
       alert('Transaction sent. Check the console for the transaction hash.');
     } catch (error) {
       console.error('Error sending transaction:', error);
@@ -87,7 +103,11 @@ export default function WalletInteraction() {
     try {
       await provider.request({ method: "wallet_switchEthereumChain", params: [{ chainId:  testnetId}] });
       const accounts = await provider.request({ method: "eth_requestAccounts" });
-      if (accounts.length > 0) setAccount(accounts[0]);
+      console.log('Connected Account:', accounts[0]);
+      if (accounts.length > 0){
+        setAccount(accounts[0]);
+        console.log('Connected Account:', accounts[0]);
+      } 
     } catch (error) {
       console.error('Error switching chains or connecting:', error);
       if (error.code === 4902) alert('Chain not found. Please add it to MetaMask.');
@@ -109,8 +129,10 @@ export default function WalletInteraction() {
       <div style={{ textAlign: 'center', marginTop: '20px' }}>
         <button onClick={sendTransaction} style={{ padding: '10px 20px', fontSize: '16px', marginRight: '10px', border: '2px solid #4CAF50', borderRadius: '5px' }}>Send</button>
         <button onClick={handleConnect} style={{ padding: '10px 20px', fontSize: '16px', border: '2px solid #008CBA', borderRadius: '5px' }}>Switch</button>
-        {account && <p style={{ backgroundColor: '#f0f0f0', padding: '10px', borderRadius: '5px', marginTop: '20px', border: '1px solid #ccc' }}>Connected Account: {account}</p>}
       </div>
     </>
   );
 }
+
+
+// {account && <p style={{ backgroundColor: '#f0f0f0', padding: '10px', borderRadius: '5px', marginTop: '20px', border: '1px solid #ccc' }}>Connected Account: {account}</p>}
